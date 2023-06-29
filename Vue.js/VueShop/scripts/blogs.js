ebe5c438f2624014cd5bc3171daabffce566d265
Alexander Sibirko <Alexandersibirko@yandex.ru>
2023-06-29 17:13:49 +0300

    added blog page with Articles created by VUE

---
+++ Vue.js/VueShop/scripts/blogs.js
@@ -0,0 +1,49 @@
+const blogs = new Vue({
+    el: "#vue-blogs",
+    data: {
+        blogsData: [
+            {
+                id: 0,
+                tagText: "Kitchen Design",
+                imageSrc: "img/articles/article1.png",
+                headerText: "Let’s Get Solution For Building Construction Work",
+                dateText: "26 December,2022",
+            },
+            {
+                id: 1,
+                tagText: "Living Design",
+                imageSrc: "img/articles/article2.png",
+                headerText: "Low Cost Latest Invented Interior Designing Ideas.",
+                dateText: "22 December,2022",
+            },
+            {
+                id: 2,
+                tagText: "Interior Design",
+                imageSrc: "img/articles/article3.png",
+                headerText: "est For Any Office & Business Interior Solution",
+                dateText: "25 December,2022",
+            },
+            {
+                id: 3,
+                tagText: "Kitchen Design",
+                imageSrc: "img/articles/article4.png",
+                headerText: "Let’s Get Solution For Building Construction Work",
+                dateText: "26 December,2022",
+            },
+            {
+                id: 4,
+                tagText: "Living Design",
+                imageSrc: "img/articles/article5.png",
+                headerText: "Low Cost Latest Invented Interior Designing Ideas.",
+                dateText: "22 December,2022",
+            },
+            {
+                id: 5,
+                tagText: "Interior Design",
+                imageSrc: "img/articles/article6.png",
+                headerText: "est For Any Office & Business Interior Solution",
+                dateText: "25 December,2022",
+            },
+        ],
+    },
+});
